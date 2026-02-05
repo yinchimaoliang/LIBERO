f0537f159146cfc4ee2a258a6872aca515e39980
liyinhao <liyinhao0413@qq.com>
2026-02-05 08:43:39 +0000

    Modify init

--- libero/libero/__init__.py
+++ libero/libero/__init__.py
@@ -61,36 +61,3 @@ def set_libero_default_path(custom_location=os.path.dirname(os.path.abspath(__fi
 
 if not os.path.exists(libero_config_path):
     os.makedirs(libero_config_path)
-
-if not os.path.exists(config_file):
-    # Create a default config file
-
-    default_path_dict = get_default_path_dict()
-    answer = input(
-        "Do you want to specify a custom path for the dataset folder? (Y/N): "
-    ).lower()
-    if answer == "y":
-        # If the user wants to specify a custom storage path, prompt them to enter it
-        custom_dataset_path = input(
-            "Enter the path where you want to store the datasets: "
-        )
-        full_custom_dataset_path = os.path.join(
-            os.path.abspath(os.path.expanduser(custom_dataset_path)), "datasets"
-        )
-        # Check if the custom storage path exists, and create if it doesn't
-
-        print("The full path of the custom storage path you entered is:")
-        print(full_custom_dataset_path)
-        print("Do you want to continue? (Y/N)")
-        confirm_answer = input().lower()
-        if confirm_answer == "y":
-            if not os.path.exists(full_custom_dataset_path):
-                os.makedirs(full_custom_dataset_path)
-            default_path_dict["datasets"] = full_custom_dataset_path
-    print("Initializing the default config file...")
-    print(f"The following information is stored in the config file: {config_file}")
-    # write all the paths into a yaml file
-    with open(config_file, "w") as f:
-        yaml.dump(default_path_dict, f)
-    for key, value in default_path_dict.items():
-        print(f"{key}: {value}")
